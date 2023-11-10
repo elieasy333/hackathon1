3f37066ceff111ab7dea52b47ced99ee75a22613
Nic <NCGThompson@gmail.com>
2023-11-10 08:30:07 -0500

    Reinitialized the project

---
+++ Hackathon_Client/Hackathon_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon_ClientApp.swift
+//  Hackathon_Client
+//
+//  Created by CS Students on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
