f1c742305818df0c0cd4d2f37c0e58e1933cd6e3
Nicholas Thompson <NCGThompson@gmail.com>
2023-11-09 20:45:07 -0500

    Made an ios app I hope

---
+++ iOS for real/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS for real
+//
+//  Created by Nicholas Thompson on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS for real/iOS_for_realApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_for_realApp.swift
+//  iOS for real
+//
+//  Created by Nicholas Thompson on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_for_realApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
