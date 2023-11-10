87fc321a18dd763ed2e31a1d75c9f7deda7694eb
elieasy333 <138052049+elieasy333@users.noreply.github.com>
2023-11-10 10:50:05 -0500

    Update MajorView.swift

--- Hackathon_Client/MajorView.swift
+++ Hackathon_Client/MajorView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct MajorView: View {
+    
+    @State var title: String = "Please Select Your Major"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:20) {
+            
+            Text(title)
+            
+            Button("Computer Science") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Computer Science"
+            }
+            Button("Compuer Engineering") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Compuer Engineering"
+            }
+            
+            Button("Mass Comunications") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Mass Comunications"
+            }
+            Button("Psycology") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Psycology"
+            }
+            Button("Criminal Justice") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Criminal Justice"
+            }
+            Button("Social Work") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Social Work"
+            }
+            Button("Liberal Arts") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = "You selected Liberal Arts"
+            }
+            Button("Business Adminstration") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                self.title = " You selected Business Adminstration"
+            }
+        }
     }
 }
 
 #Preview {
-    MajorView()
-}
+        MajorView()
+    }
+    
